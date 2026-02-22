5da2b2e8412053ed33bfce9227a533bfc9212fb9
Elecorde <elecorde@ucm.es>
2026-02-22 20:06:24 +0100

    Fotos
    Fotos para la página principal y un pequeño cambio en la cabecera (se puede quitar si no gusta)

--- practica2/includes/vistas/comun/cabecera.php
+++ practica2/includes/vistas/comun/cabecera.php
@@ -1,4 +1,7 @@
 <div class="cabecera" style="display:flex;align-items:center;justify-content:space-between;background-color:green;height:10vh">
+    <div > 
+        <img src="img/uploads/bistroFDILogo.png" width="80px" alt="Logo de Bistro FDI"> 
+    </div>
     <div style="text-align:left;">
         <h1 >Bistro FDI</h1>
     </div>
