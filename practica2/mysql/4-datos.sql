2cbf6d1df96932f2cd0c0c082e41ef851cb6ab3f
jpolo05 <jpolo05326sancho@gmail.com>
2026-03-09 21:38:28 +0100

    bd

--- practica2/mysql/4-datos.sql
+++ practica2/mysql/4-datos.sql
@@ -18,9 +18,12 @@ INSERT INTO `productos` (`nombre`, `descripcion`, `id_categoria`, `precio_base`,
 ('Hamburguesa completa', 'Hamburguesa con queso y patatas', 2, 12.00, 10, 1, 1, 'img/uploads/productos/hamburguesa.jpg'),
 ('Tarta de queso', 'Tarta casera al horno', 3, 4.50, 10, 1, 1, 'img/uploads/productos/tarta_queso.jpg');
 
-INSERT INTO `usuarios` (`user`, `email`, `nombre`, `apellidos`, `contrasena`, `rol`, `imagen`) VALUES
-('camarero', 'camarero@gmail.com', 'camarero', 'camarero', '$2y$10$pw0Ms551XxFrg4SmMOBz/O/8504T24xICATLSCb7Yn4LZoLoxRtwO', 'Camarero', 'img/uploads/usuarios/default.jpg'),
-('cliente', 'cliente@gmail.com', 'cliente', 'cliente', '$2y$10$.YAs9G8w5.R0il57XaZrcuC582ZyN0sBTiJTS9bt/gXXqZtBSxsaK', 'Cliente', 'img/uploads/usuarios/default.jpg'),
-('cocinero', 'cocinero@gmail.com', 'cocinero', 'cocinero', '$2y$10$kDvu6.r0F3ZuIkBJB3/6x.hoJ07OV5HOG05ws3tniErZK/bW.21A6', 'Cocinero', 'img/uploads/usuarios/default.jpg'),
-('gerente', 'gerente@gmail.com', 'gerente', 'gerente', '$2y$10$.ZoCF3y/IuO5gUkdYAR.JuiVaLSs0k6oMmeiUMAToRe.H0ZJQZ5ae', 'Gerente', 'img/uploads/usuarios/default.jpg');
+INSERT INTO `pedidos` (`id`, `numeroPedido`, `estado`, `tipo`, `fecha`, `cliente`, `cocinero`, `imagenCocinero`, `total`) VALUES
+(1, 1, 'En preparación', 'Local', '2026-03-09 19:59:54', 'cliente', NULL, 'img/uploads/usuarios/default.jpg', 0.00),
+(2, 2, 'En preparación', 'Local', '2026-03-09 20:00:05', 'cliente', NULL, 'img/uploads/usuarios/default.jpg', 0.00),
+(3, 3, 'Cocinando', 'Local', '2026-03-09 20:00:16', 'cliente', 'cocinero', 'img/uploads/usuarios/default.jpg', 0.00);
 
+INSERT INTO `linea_pedido` (`numeroPedido`, `idProducto`, `cantidad`, `subtotal`, `estado`) VALUES
+(3, 1, 1, 0.00,0),
+(3, 2, 1, 0.00,0),
+(3, 3, 1, 0.00,0);
