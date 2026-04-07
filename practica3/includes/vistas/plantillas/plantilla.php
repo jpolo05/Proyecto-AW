b67e470637fae1cee0a03b9fe29e758b4fa211ee
jpolo05 <jpolo05326sancho@gmail.com>
2026-04-07 17:31:41 +0200

    para moviles

--- practica3/includes/vistas/plantillas/plantilla.php
+++ practica3/includes/vistas/plantillas/plantilla.php
@@ -5,6 +5,7 @@
 <html lang="es">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $tituloPagina ?></title>
         <link rel="stylesheet" href="<?= RUTA_CSS.'layout.css' ?>">
         <link rel="stylesheet" href="<?= RUTA_CSS.'componentes.css' ?>">
