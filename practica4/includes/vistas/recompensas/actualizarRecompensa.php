5e4200bd27120851d6bedd102490cb4ed8a7d5ad
aravilla11 <aravilla@ucm.es>
2026-04-23 20:51:08 +0200

    Update actualizarRecompensa.php

--- practica4/includes/vistas/recompensas/actualizarRecompensa.php
+++ practica4/includes/vistas/recompensas/actualizarRecompensa.php
@@ -8,3 +8,12 @@ Auth::verificarAcceso('Gerente');
 
 $id = (int)($_GET['id'] ?? 0);
 $recompensa = $id > 0 ? Recompensa::buscaPorId($id) : null;
+
+if (!$recompensa) {
+    header('Location: '.RUTA_APP.'includes/vistas/recompensas/listarRecompensas.php?msg=Recompensa+no+encontrada');
+    exit;
+}
+
+$error = '';
+$csrfToken = Auth::getCsrfToken();
+
