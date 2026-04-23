b94523f51e92b2ccff192df41b96ea763ab8863f
aravilla11 <aravilla@ucm.es>
2026-04-23 20:45:00 +0200

    Update visualizarRecompensa.php

---
+++ practica4/includes/vistas/recompensas/visualizarRecompensa.php
@@ -0,0 +1,12 @@
+<?php
+use es\ucm\fdi\aw\usuarios\Auth;
+use es\ucm\fdi\aw\usuarios\Recompensa;
+use es\ucm\fdi\aw\usuarios\Producto;
+
+require_once __DIR__.'/../../config.php';
+
+$id = (int)($_GET['id'] ?? 0);
+if ($id <= 0) {
+    header('Location: '.RUTA_APP.'includes/vistas/recompensas/listarRecompensas.php?msg=Recompensa+invalida');
+    exit;
+}
