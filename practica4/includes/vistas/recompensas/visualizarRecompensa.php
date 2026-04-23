8d428023ff0391f59248e623eb947b1561968c4c
aravilla11 <aravilla@ucm.es>
2026-04-23 20:48:37 +0200

    Update visualizarRecompensa.php

--- practica4/includes/vistas/recompensas/visualizarRecompensa.php
+++ practica4/includes/vistas/recompensas/visualizarRecompensa.php
@@ -10,3 +10,10 @@ if ($id <= 0) {
     header('Location: '.RUTA_APP.'includes/vistas/recompensas/listarRecompensas.php?msg=Recompensa+invalida');
     exit;
 }
+
+$recompensa = Recompensa::buscaPorId($id);
+if (!$recompensa) {
+    header('Location: '.RUTA_APP.'includes/vistas/recompensas/listarRecompensas.php?msg=Recompensa+no+encontrada');
+    exit;
+}
+
